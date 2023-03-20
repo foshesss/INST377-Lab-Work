98efdedfb47303cbd01ddfc2d82bc7776fa1b651
foshesss <foshessss@gmail.com>
2023-03-20 00:10:44 -0400

    lab 6

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -12,21 +12,18 @@
 */
 
 function injectHTML(list) {
-  console.log('fired injectHTML');
-  /*
-  ## JS and HTML Injection
-    There are a bunch of methods to inject text or HTML into a document using JS
-    Mainly, they're considered "unsafe" because they can spoof a page pretty easily
-    But they're useful for starting to understand how websites work
-    the usual ones are element.innerText and element.innerHTML
-    Here's an article on the differences if you want to know more:
-    https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
+    console.log('fired injectHTML');
 
-  ## What to do in this function
-    - Accept a list of restaurant objects
-    - using a .forEach method, inject a list element into your index.html for every element in the list
-    - Display the name of that restaurant and what category of food it is
-*/
+    const restaurantList = document.querySelector("#restaurant_list")
+
+    // convert list to an ordered list with list elements
+    const content = "<ol>" + 
+      list.reduce((accumulator, {name, category}) => 
+        `${accumulator} <li>${name}, ${category}</li>`, ""
+      ) + "</ol>"
+
+    // set innerHTML to content, which is an ordered list of restaurants
+    restaurantList.innerHTML = content
 }
 
 function processRestaurants(list) {
@@ -50,6 +47,31 @@ function processRestaurants(list) {
     - Return only their name, category, and location
     - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
   */
+
+    const listLen = list.length
+    // create an empty array with 15 empty dicts
+    let restaurants = Array.apply(null, Array(15)).map(() => { return {} }) 
+
+    // create a set of selected numbers to ensure there are no duplicate restaurants
+    const selectedNumbers = new Set()
+    restaurants.forEach(info => {
+      // find a restaurant to insert
+      let index // define outside of 'do while' to be used later
+      do {
+        index = Math.floor(Math.random()*listLen)
+      } while (selectedNumbers.has(index))
+
+      selectedNumbers.add(index) // prevent using same index again
+
+      // info is a reference copy, so modifying 'info' in this function will
+      // also modify the dict within the restaurant array
+      const selectedRestaurant = list[index]
+      info.name = selectedRestaurant.name
+      info.category = selectedRestaurant.category
+      info.location = selectedRestaurant.location
+    })
+
+    return restaurants
 }
 
 async function mainEvent() {
@@ -79,12 +101,17 @@ async function mainEvent() {
 
   // As a next step, log the first entry from your returned array of data.
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
-  console.log(`replace me with the first entry`);
+  console.log(arrayFromJson[0]);
 
   // Now write a log using string interpolation - log out the name and category of your first returned entry (index [0]) to the browser console
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Property_accessors
-  console.log(`replace me with the name and category of the first entry`);
+  console.log(`name: ${arrayFromJson[0].name}, category: ${arrayFromJson[0].category}`);
+
+  // get loading anim element
+  const loadingElem = document.querySelector(".lds-ellipsis");
+  loadingElem.style.display = "none"
+
 
   // This IF statement ensures we can't do anything if we don't have information yet
   if (arrayFromJson?.length > 0) { // the question mark in this means "if this is set at all"
@@ -92,15 +119,27 @@ async function mainEvent() {
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
+
+    let lastClick = null// this just initializes the value
     form.addEventListener('submit', (submitEvent) => {
       // Using .preventDefault, stop the page from refreshing when a submit event happens
       // https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
+      submitEvent.preventDefault();
 
+      // used as a sanity check so someone that spams submit doesn't break the page
+      const currentClick = new Date()
+      lastClick = currentClick
+
+      loadingElem.style.display = "inline-block"
       // This constant will contain the value of your 15-restaurant collection when it processes
       const restaurantList = processRestaurants(arrayFromJson);
 
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(restaurantList);
+      setTimeout(() => {
+        if (currentClick != lastClick) return // this means a new click occurred before we could load the lsit
+
+        loadingElem.style.display = "none"
+        injectHTML(restaurantList);
+      }, 1000);
     });
   }
 }
